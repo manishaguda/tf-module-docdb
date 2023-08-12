d62efdc1a3a2a71c3da42bbc3f39b9100f5676e8
manisha guda <manishavguda@gmail.com>
2023-08-12 10:12:38 +0530

    terraform

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "aws_docdb_cluster" "docdb" {
   engine                  = "docdb"
   master_username         = "foo"
   master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
+  db_subnet_group_name    = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids  = [aws_security_group.docdb.id]
 
   tags = merge(
     local.common_tags,
